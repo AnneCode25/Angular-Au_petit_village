367c8bc7648c79e66be8acfda524bf7ee89f6e69
AnneVi <villette55@gmail.com>
2024-07-23 14:45:35 +0200

    ajout des cards produits

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { ProductService, Product } from '../product.service';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
 CompanyName='Au petit village';
 CompanyShortDescription='Au petit village est une entreprise qui confectionne des figurines inspirées de la bande dessinée Astérix et Obélix.'
+products: Product[] = [];
+
+constructor(private productService: ProductService) { }
+
+ngOnInit() {
+  this.productService.getProducts().subscribe(products => this.products = products);
+}
+
 }
