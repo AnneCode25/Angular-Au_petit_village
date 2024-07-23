d7b464efb051556ec71271795e271e22f4b5ba89
AnneVi <villette55@gmail.com>
2024-07-23 15:59:08 +0200

    ajout page A propos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
         { path: '', component: HomePageComponent},
         { path: 'product/:id', component: ProductDetailComponent },
-        // { path: '/about', component: AboutComponent}
+        { path: 'about', component: AboutComponent}
 
 ];
