f042fef109a653f5c80be02420a452fdf46fb6a0
AnneVi <villette55@gmail.com>
2024-07-23 13:50:34 +0200

    ajout 1ere partie page d'accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
-        // { path: '', component: HomeComponent}, 
+        { path: '', component: HomePageComponent}, 
         // { path: '/about', component: AboutComponent}
 
 ];
