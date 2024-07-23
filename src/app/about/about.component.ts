85e3b2dcfb1862beab44b74d780dff0dafd3fdc4
AnneVi <villette55@gmail.com>
2024-07-23 16:12:21 +0200

    ajout utilisation du service dans la page a propos

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService, Product } from '../product.service'
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  featuredProducts: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      // Par exemple, sélectionnez les 3 premiers produits comme produits vedettes
+      this.featuredProducts = products.slice(0, 3);
+    });
+  }
 }
